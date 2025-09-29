76fdaec26d71af48b08cb1c87c8c5169709fe12f
Ranjot Singh Singh <rsingh18@rrc.ca>
2025-09-28 22:15:42 -0500

    add server entry point

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+// src/server.ts
+import app from "./app";
+
+const PORT: string | number = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
