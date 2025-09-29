a86ad8e5bbe7c018cb82fbc183e6366e38c5b256
Ranjot Singh Singh <rsingh18@rrc.ca>
2025-09-28 22:40:15 -0500

    configure jest for typescript testing

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/server.ts',
+  ],
+};
