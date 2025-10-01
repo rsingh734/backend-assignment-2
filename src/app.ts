83a771e7157480bf28cd3b8ee004b2408ae71090
Ranjot Singh Singh <rsingh18@rrc.ca>
2025-10-01 18:46:20 -0500

    register employee routes in main application

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // src/app.ts
 import express, { Express } from "express";
 import morgan from "morgan";
+import employeeRoutes from "./api/v1/routes/employeeRoutes";
 
 const app: Express = express();
 
@@ -14,6 +15,8 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.status(200).send("Server is healthy");
 });
+
+app.use("/api/v1", employeeRoutes);
 
 // Export the app
 export default app;
