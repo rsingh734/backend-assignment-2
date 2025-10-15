81ac4d0538f785468c02d1228b32e0c4811e4e78
Ranjot Singh Singh <rsingh18@rrc.ca>
2025-10-14 23:28:48 -0500

    Final step testing everything

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // src/server.ts
 import app from "./app";
 
-const PORT: string | number = process.env.PORT || 3000;
+const PORT: string | number = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
